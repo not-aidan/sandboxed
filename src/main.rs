2ce569b01de0e4ac3a748a82d840e4bccd253ff0
not-aidan <jclakeaidan@gmail.com>
2023-02-22 11:17:10 -0500

    Rendered blank window

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@ use winit::{
     window::WindowBuilder,
 };
 
-fn main() {
+mod renderer;
+use renderer::Renderer;
+
+#[tokio::main]
+async fn main() -> Result<(), ()> {
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut renderer = Renderer::new(window).await;
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
+        } if window_id == renderer.window().id() => match event {
             WindowEvent::CloseRequested
             | WindowEvent::KeyboardInput {
                 input:
@@ -26,6 +31,22 @@ fn main() {
             } => *control_flow = ControlFlow::Exit,
             _ => {}
         },
+        Event::RedrawRequested(window_id) if window_id == renderer.window().id() => {
+            match renderer.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => renderer.resize(renderer.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            renderer.window().request_redraw();
+        }
         _ => {}
     });
 }
